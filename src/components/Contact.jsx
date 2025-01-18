3683431e73573296d62d6e8fcd68d28e9e851530
akashkumaryadav <ay355231@gmail.com>
2025-01-18 05:51:05 +0530

    Added all the sections

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -2,7 +2,13 @@ import React from 'react'
 
 const Contact = () => {
   return (
-    <div>Contact</div>
+    <div id='contact' className='flex min-h-screen
+    w-full flex-col items-center justify-center
+    gap-16 p-8'>
+     <h1 className='text-center text-6xl font-light 
+     text-teal-600'>Get in Touch</h1>
+
+    </div>
   )
 }
 
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,24 +1,54 @@
 import React from 'react'
 import image from '../../public/image.jpg'
+import { BiLogoGithub, BiLogoLinkedin, BiLogoTwitter, BiLogoYoutube } from 'react-icons/bi'
+import { SiLeetcode } from 'react-icons/si';
 const Home = () => {
   return (
     <div id='home' className='flex min-h-screen w-full items-center justify-center'>
 
     <div className='flex flex-col items-center 
-    justify-center gap-8 p-5 text-center'></div>
-     <img src={image} alt="" className='w-[250px]
+    justify-center gap-8 p-5 text-center'>
+      <img src={image} alt="" className='w-[250px]
      sm:w-[300px] rounded-full'/>
      <div className="space-y-1 sm:space-y-3">
       <h1 className='bg-gradient-to-r from-teal-400
       to-teal-600 bg-clip-text text-4xl font-semibold text-transparent
       md:text-5xl lg:texxt-6xl'>Akashkumar Yadav</h1>
       <h3 className='bg-gradient-to-r from-teal-400 to-teal-600
-      bg-clip-text text-4xl font-semibold text-transparent md:text-5xl lg:text-6xl'>Software Developer</h3>
-      <p>
-      
+      bg-clip-text text-xl font-semibold text-transparent md:text-2xl lg:text-3xl'>
+        Software Developer
+        </h3>
+      <p className='max-w-[500px] text-sm text-gray-500'>
+      Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+      Placeat, vero. Temporibus ratione iure earum reprehenderit!
+      Dolores blanditiis quia debitis quidem.
       </p>
      </div>
 
+     <div className="flex gap-3">
+
+         <BiLogoGithub className='h-10 w-10 cursor-pointer rounded-full border-2 border-transparent bg-teal-600 p-2
+         text-white transition-all duration-200 
+         hover:scale-110 hover:border-teal-600 
+         hover:bg-white hover:text-teal-600 
+         md:h-12 md:w-12'/>
+         <BiLogoLinkedin className='h-10 w-10 cursor-pointer rounded-full border-2 border-transparent bg-teal-600 p-2
+         text-white transition-all duration-200 
+         hover:scale-110 hover:border-teal-600 
+         hover:bg-white hover:text-teal-600 
+         md:h-12 md:w-12'/>
+         <SiLeetcode className="h-10 w-10 cursor-pointer rounded-full border-2 border-transparent bg-teal-600 p-2
+        text-white transition-all duration-200 
+        hover:scale-110 hover:border-teal-600 
+        hover:bg-white hover:text-teal-600 
+        md:h-12 md:w-12" />
+        <BiLogoTwitter className='h-10 w-10 cursor-pointer rounded-full border-2 border-transparent bg-teal-600 p-2
+         text-white transition-all duration-200 
+         hover:scale-110 hover:border-teal-600 
+         hover:bg-white hover:text-teal-600 
+         md:h-12 md:w-12'/>
+     </div>
+    </div>
     </div>
   )
 }
