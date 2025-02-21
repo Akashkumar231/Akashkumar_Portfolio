3eedef00f1846bd1c872c63907ed6751de63f6a4
Akashkumar231 <101165107+Akashkumar231@users.noreply.github.com>
2025-02-21 23:57:56 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import HeaderLeft from './components/HeaderLeft'
 import Home from './components/Home'
 import Experience from './components/Experience'
 import Projects from './components/Projects'
-import Resume from './components/Resume'
+import Resume from './components/Resume'\
 
 function App() {
   return (
@@ -11,9 +11,13 @@ function App() {
        <HeaderLeft />
        <main className="flex-[3]">
          <Home />
+         
          <Experience />
+         
          <Projects />
+         
          <Resume />
+         
        </main>
       </div>
     </>
